b023b26dc9e6b5fcebc8e677ea322f95afc79b79
elenagernichenko <elena.gernichenko@yandex.ru>
2023-10-23 21:18:53 +0300

    added lvl 3 task

--- data.sql
+++ data.sql
@@ -1,17 +1,46 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES
+    (1, 'Program'),
+    (2, 'Sub Program'),
+    (3, 'Project'),
+    (4, 'Contract'),
+    (5, 'Point');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES
+    (1, NULL, 1, 'Программа 1', NULL),
+    (2, NULL, 1, 'Программа 2', NULL),
+    (3, 1, 2, 'Подпрограмма 1.1', NULL),
+    (4, 1, 2, 'Подпрограмма 1.2', NULL),
+    (5, 3, 3, 'Проект 1.1.1', NULL),
+    (6, 3, 3, 'Проект 1.1.2', NULL),
+    (7, 5, 5, 'КТ (проекта) 1.1.1.1', NULL);
+
+INSERT INTO Program (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (6);
+
+INSERT INTO SubProgram (ActivityId)
+VALUES (4),
+       (5);
+
+INSERT INTO Project (ActivityId)
+VALUES (2),
+       (3),
+       (4),
+       (5);
+
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 1),
+       (4, 2),
+       (6, 3);
+
+INSERT INTO Point (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
